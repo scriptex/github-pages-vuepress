5f90f1e51f5930356043dccabae8cc3058891af5
Atanas Atanasov <scriptex.bg@gmail.com>
2018-06-13 13:18:42 +0300

    Add VuePress config

---
+++ .vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	base: '/',
+	title: 'VuePress Example in Github Pages',
+	description:
+		'Static website built with VuePress and deployed on Github Pages',
+	dest: './public'
+};
