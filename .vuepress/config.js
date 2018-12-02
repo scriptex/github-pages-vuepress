da313370f8b3b6df1626a0765da5bf5d1aa74742
Atanas Strahilov <atanass@eondata.onmicrosoft.com>
2018-12-02 14:58:14 +0200

    Update config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	base: '/github-pages-vuepress/',
-	title: 'VuePress Example in Github Pages',
-	description:
-		'Static website built with VuePress and deployed on Github Pages',
+	title: 'VuePress in Github Pages',
+	description: 'Static website built with VuePress and deployed on Github Pages',
 	dest: './docs'
 };
